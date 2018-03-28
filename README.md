85c5374299842fbc0d62348d8cf57eb50754c65c
xiyaoguo <xiyaoguo@yeah.net>
2018-03-28 14:08:31 +0800

    xiyaoguo

--- README.md
+++ README.md
@@ -178,7 +178,7 @@ Input is guaranteed to be within the range from 1 to 3999.
 
 Write a function to find the longest common prefix string amongst an array of strings.
 
-### #14.[3Sum](https://leetcode.com/problems/3sum/description/)
+### #15.[3Sum](https://leetcode.com/problems/3sum/description/)
 
 Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
 
---
+++ src/main/java/com/fudanse/leetcode/No15.java
@@ -0,0 +1,45 @@
+package com.fudanse.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class No15 {
+	public static List<List<Integer>> threeSum(int[] nums) {
+		Set<List<Integer>> res = new HashSet<>();
+		if (nums == null || nums.length < 3)
+			return new ArrayList<>();
+		Arrays.sort(nums);
+		int i = 0;
+
+		while (i < nums.length - 2) {
+			if (i == 0 || nums[i] != nums[i - 1]) {
+				int l = i + 1, h = nums.length - 1;
+				int sum = 0 - nums[i];
+				while (l < h) {
+					if (nums[l] + nums[h] < sum)
+						l++;
+					else if (nums[l] + nums[h] > sum)
+						h--;
+					else {
+						res.add(Arrays.asList(nums[i], nums[l], nums[h]));
+						h--;
+						l++;
+					}
+				}
+			}
+			i++;
+		}
+
+		return new ArrayList<>(res);
+	}
+
+	public static void main(String[] args) {
+
+		List<List<Integer>> res = threeSum(new int[] { -1, 0, 1, 2, -1, -4 });
+		res.forEach((n) -> System.out.println(n.toString()));
+	}
+
+}
